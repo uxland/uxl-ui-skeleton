24a7a53614c11e21cf95178afa2a9f964b99010b
danielcabiscol <danielcabiscol@gmail.com>
2019-01-24 13:03:11 +0100

    perf(): upgrade to LitElement 2.0.0rc3
    BREAKING CHANGE: Now "number" property is available to accept a string or number, like "5" or 5

--- src/uxl-ui-skeleton.ts
+++ src/uxl-ui-skeleton.ts
@@ -1,5 +1,5 @@
 import {html, LitElement} from 'lit-element/lit-element';
-import {property, customElement} from "@uxland/uxl-polymer2-ts";
+import {property, customElement} from "lit-element/lib/decorators";
 import CSS from './uxl-ui-skeleton-styles.js';
 import {template as TEMPLATE} from './uxl-ui-skeleton-template.js';
 import {skeletonAnimation, skeletonType} from "./index";
@@ -12,14 +12,17 @@ export class UxlUiSkeleton extends LitElement {
     }
 
      get items(){
-        return this.number && new Array(this.number);
+        if(typeof this.number == "string")
+            return this.number && Array.from(Array(parseInt(this.number)).keys());
+        else
+            return this.number && Array.from(Array(this.number).keys());
      }
 
     @property()
     type: skeletonType;
 
     @property()
-    number: number;
+    number: string;
 
     @property()
     animation: skeletonAnimation = "intermitent";
